0f59ea6494d630652eb92d2039d9fd89231bd374
Bran <bbogdanich42@gmail.com>
2020-10-29 14:52:35 -0300

    1

--- src/componentes/ui/Miembros.js
+++ src/componentes/ui/Miembros.js
@@ -30,23 +30,7 @@ export default class Miembros extends Component {
                         <div className="col">
                             <LocalidadContenido />
                         </div>
-                        <div className="col col-lg-5">
-                            <Accordion className="shadow" >
-                                <Card>
-                                    <Card.Header style={{background: "#ccc"}}>
-                                        <Accordion.Toggle as={Card.Header} eventKey="0" style={{background: "#80B8C8"}}>
-                                            <p className="text-center text-uppercase" style={{fontFamily: "knewave", color:"#15343D", fontSize:"30px", cursor:"pointer"}}>Ver Mapa</p>
-                                        </Accordion.Toggle>
-                                    </Card.Header>
-                                    <Accordion.Collapse eventKey="0">
-                                        <Card.Body style={{background: "linear-gradient(135deg, rgba(4,189,96,1) 0%, rgba(54,170,224,1) 73%, rgba(54,170,224,1) 100%)"}}>
-                                            <Mapa />
-                                        </Card.Body>
-                                    </Accordion.Collapse>
-                                </Card>
-                            </Accordion>
-                        </div>
-                    
+                      
                     </div>
                 </div>
             </div>

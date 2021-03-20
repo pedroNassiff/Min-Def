0b3394539ba02818e1c708f4a1e491ed3d107fc6
Bran <bbogdanich42@gmail.com>
2021-03-20 14:43:25 -0300

    upload

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -11,22 +11,20 @@ router.use(function(req, res, next) {
 });
 
 //cargar archivos solo si esta logeado 
-router.post('/upload',[
-	
-], async (req, res) => {
+router.post('/upload', async (req, res) => {
 	
 	if(req.files === null) {
         return res.status(400).json({msg: 'Archivo no subido'});
     }
     const file =  req.files.file;
-
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send(err);
-        }
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+	file.mv(`/var/www/vhosts/mpdchaco.com.ar/httpdocs/client/build/uploads/${file.name}`, err => {
+		if (err) {
+			console.log(err);
+			return res.status(500).send(err);
+		}
+		res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
     });
+    
 
 })
 
